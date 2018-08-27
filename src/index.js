dd3ab67e2896fc54a72855c0d16dbccfbb802193
mahongquan <mahongquan@sina.com>
2018-08-27 21:57:51 +0800

    build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Browser from './AppScrum';
+import App from './AppScrum';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'react-tabs/style/react-tabs.css';
+import './style.css';
+import './animate.min.css';
 //import ContextMenuExample from './ContextMenuExample';
-ReactDOM.render(<Browser />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
